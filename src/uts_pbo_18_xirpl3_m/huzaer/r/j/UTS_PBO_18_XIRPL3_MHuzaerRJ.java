827b92dca10da9174b65185e86d65918337183be
HuzaerRekso <muhammadhrj0@gmail.com>
2017-03-17 22:55:19 +0700

    Commit 1

---
+++ src/uts_pbo_18_xirpl3_m/huzaer/r/j/UTS_PBO_18_XIRPL3_MHuzaerRJ.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts_pbo_18_xirpl3_m.huzaer.r.j;
+
+/**
+ *
+ * @author MHRJ
+ */
+public class UTS_PBO_18_XIRPL3_MHuzaerRJ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
